37eea1495afbb7bbf0731f464662a64e274f554a
Arnout Boks <arnout.boks@moxio.com>
2021-07-27 10:01:28 +0200

    Add Config implementation for when psalm.xml is missing

---
+++ src/PsalmConfig/MissingConfig.php
@@ -0,0 +1,12 @@
+<?php
+declare(strict_types=1);
+
+namespace Moxio\CaptainHook\Psalm\PsalmConfig;
+
+class MissingConfig implements Config
+{
+    public function belongsToProjectFiles(string $relativePath): bool
+    {
+        return true;
+    }
+}
